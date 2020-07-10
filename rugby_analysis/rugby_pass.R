8609c54a65dd4a6e6e40a0b5411327038ed7fd39
Oliver Eaton <oliverf.eaton@gmail.com>
2020-07-10 17:58:33 +1000

    jul 10

--- rugby_analysis/rugby_pass.R
+++ rugby_analysis/rugby_pass.R
@@ -12,45 +12,48 @@ library(lubridate)
 library(tibble)
 library(purrr)
 
-setwd("C:/Users/olive/Desktop/N-SYNC")
 source("rugby_pass_scrape_functions.R")
 
 paths_allowed(paths = "https://index.rugbypass.com/") 
 rPass <- read_html("https://index.rugbypass.com/")
 
-tournament_info <- rPass %>% html_nodes("#tournaments-drop-down") %>% html_nodes("li")
-tournament_id <- tournament_info %>% html_attr(name="data-value")
-tournament_name <- tournament_info %>% html_text() %>% str_trim()
+# Get tournament information
+tournament_id <- rPass %>% html_nodes("#tournaments-drop-down") %>% html_nodes("li") %>% html_attr(name="data-value")
+tournament_name <- rPass %>% html_nodes("#tournaments-drop-down") %>% html_nodes("li") %>% html_text() %>% str_trim()
 season <- "Jun-Aug 2020"
 tournaments <- as.data.frame(cbind(tournament_id, tournament_name, season))
 
-team_info <- rPass %>% html_nodes("#teams-drop-down") %>% html_nodes("li")
-team_id <- team_info %>% html_attr(name="data-value")
-team_name <- team_info %>% html_text()  %>% str_trim()
-tournament_id <- team_info %>% html_attr(name="data-tournaments")
+# Get team information
+team_id <- rPass %>% html_nodes("#teams-drop-down") %>% html_nodes("li") %>% html_attr(name="data-value")
+team_name <- rPass %>% html_nodes("#teams-drop-down") %>% html_nodes("li") %>% html_text()  %>% str_trim()
+tournament_id <- rPass %>% html_nodes("#teams-drop-down") %>% html_nodes("li") %>% html_attr(name="data-tournaments")
 teams <- as.data.frame(cbind(team_id, team_name, tournament_id))
 teams <- separate_rows(teams, tournament_id)
 
 df <- left_join(teams, tournaments, by = "tournament_id")
-df <- df[, c("season", "team_tournament_id", "tournament_name", "team_id", "team_name")] %>% 
-  subset(grepl("aotearoa", df$tournament_name, ignore.case = TRUE) & team_name == "Blues")
+df <- df[, c("season", "tournament_id", "tournament_name", "team_id", "team_name")] %>% 
+  subset(grepl("aotearoa", df$tournament_name, ignore.case = TRUE))
 
+# Get player information
+players <- data.frame()
 for (tournament_row in 1:nrow(df)) {
   tournament <- df[tournament_row, "tournament_name"]
   team <- df[tournament_row, "team_name"]
   URL <- paste0("https://index.rugbypass.com/rpi/", tournament, "/", team, "/all/7-days/high-to-low/players/")
   
   # call player name function()
-  players <- player_name(URL)
+  players_temp <- player_name(URL)
   
   # Reformat variable names for next url
   tournament <- tournament %>% str_to_lower() %>% str_replace_all(" ", "-")
   team <- team %>% str_to_lower() %>% str_replace_all(" ", "-")
+  
+  # Emtpy dataframe for loop
   p_bio <- data.frame()
   
-  for (player_row in 1:nrow(players)) {
-    id <- players[player_row, "player_id"]
-    name <- players[player_row, "player_name"] %>% str_to_lower() %>% str_replace_all(" ", "-")
+  for (player_row in 1:nrow(players_temp)) {
+    id <- players_temp[player_row, "player_id"]
+    name <- players_temp[player_row, "player_name"] %>% str_to_lower() %>% str_replace_all(" ", "-")
     URL <- paste0("https://index.rugbypass.com/rpi/", tournament, "/", team, "/", name)
     
     # call player bio function
@@ -60,17 +63,25 @@ for (tournament_row in 1:nrow(df)) {
     if(length(p_bio_tmp) == 1){
       colnames(p_bio_tmp) <- "player_id"
     } else {
-      colnames(p_bio_tmp) <- c("player_id", "position", "height", "weight", "dob")
+      colnames(p_bio_tmp) <- c("player_id", "position", "height_m", "weight_kg", "dob_ymd")
     }
-    rownames(p_bio_tmp) <- player_row
-    
     # collate rows
     p_bio <- bind_rows(p_bio, p_bio_tmp)
   }
-  # Join p_bio onto players df
-  players <- left_join(players, p_bio, by = "player_id")
-  players$team_id <- as.list(df[tournament_row, "team_id"])
-  players$season <- as.list(df[tournament_row, "season"])
-  players <- players %>% select(season, team_id, player_id, player_name, position, dob, height, weight)
+  p_bio$team_id <- as.list(df[tournament_row, "team_id"])
+  p_bio$season <- as.list(df[tournament_row, "season"])
+  players_temp <- left_join(players_temp, p_bio, by = "player_id")
+  players <- rbind(players, players_temp)
 }
+
+# Cleaning
+players <- players %>% select(season, team_id, player_id, player_name, position, dob_ymd, height_m, weight_kg)
+players$weight_kg <- players$weight_kg %>% str_replace("kg", "")
+players$height_m <- players$height_m %>% str_replace("m", "")
+players <- na_if(players, "")
+players <- na_if(players, "---")
+players$height_m <- as.double(players$height_m)
+players$weight_kg <- as.integer(players$weight_kg)
+players$dob_ymd <- as.Date(players$dob_ymd, format = "%d %b %Y")
+
 
