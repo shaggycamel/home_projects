5d6407ca5fcfd3df200aa28af46c350f07a858d2
shaggycamel <eat_fred@protonmail.com>
2021-05-23 22:20:58 +1000

    add log file

--- political_twitter/data/data_processing.R
+++ political_twitter/data/data_processing.R
@@ -7,6 +7,9 @@
 
 # Environ -----------------------------------------------------------------
 
+# Start timer
+start_time <- Sys.time()
+
 library(tidyverse)
 library(rtweet)
 library(RMySQL)
@@ -43,3 +46,31 @@ write_to_db(influencer_twitter_details)
 write_to_db(influencer_tweets)
 write_to_db(influencer_friends)
 write_to_db(influencer_mentions)
+
+
+# Log File ----------------------------------------------------------------
+
+# End timer
+end_time <- Sys.time()
+
+# Print number of rows to log file
+write(
+  paste(
+    Sys.Date()
+    , round(end_time - start_time, 2)
+    , nrow(influencer_id)
+    , nrow(influencer_twitter_details)
+    , nrow(influencer_tweets)
+    , nrow(influencer_friends)
+    , nrow(influencer_mentions)
+    , sep = "\t"
+  )
+  , file = here::here(".log")
+  , append = TRUE
+)
+
+# Print complete message
+print("political_twitter database updated...")
+
+
+
