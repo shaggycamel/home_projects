4ff2ec35f131c1a153c4e30a1bf0f950cb801927
shaggycamel <eat_fred@protonmail.com>
2021-09-09 21:53:06 +1000

    kmeans

--- political_twitter/analysis/hashtag.R
+++ political_twitter/analysis/hashtag.R
@@ -9,6 +9,7 @@
 library(tidyverse)
 library(tidytext)
 library(textclean)
+library(widyr)
 
 # Database functions
 source(here::here("data", "preprocessing", "database_functions.R"))
@@ -22,15 +23,18 @@ influencer_mentions <- read_table("influencer_mentions")
 
 # Working df
 df <- bind_rows(
-  distinct(influencer_tweets, status_id, text)
-  , distinct(influencer_mentions, status_id, text)
+  distinct(filter(influencer_tweets, created_at >= '2021-08-01'), status_id, text)
+  , distinct(filter(influencer_mentions, created_at >= '2021-08-01'), status_id, text)
 ) |> 
   filter(str_detect(text, "#"))
+
 
 # hashtag df
 hashtag_df <- unnest_tweets(df, input = text, output = hashtag) |> 
   filter(str_starts(hashtag, "#")) |> 
   distinct()
+# Filter out hashtags with low count
+# Remove emoji from hashtags
 
 # word df
 word_df <- mutate(df, language = cld2::detect_language(text)) |> 
@@ -38,26 +42,43 @@ word_df <- mutate(df, language = cld2::detect_language(text)) |>
   mutate(
     text = str_replace_all(text, "&amp;", "and")
     , text = str_remove_all(text, "(@|#)[_a-z0-9]+")
-    , text = replace_names(text)
+    # , text = replace_names(text)
     , text = replace_email(text)
-    , text = replace_non_ascii(text)
+    # , text = replace_non_ascii(text)
     , text = replace_word_elongation(text)
     , text = replace_contraction(text)
-    , text = replace_hash(text)
+    # , text = replace_hash(text)
     , text = str_remove_all(text, "[:digit:]")
   ) |> 
   na.omit() |> 
   filter(strip(text) != "") |> 
   select(-language) |> 
   unnest_tweets(input = text, output = word, strip_url = TRUE) |> 
-  anti_join(stop_words, by = "word") |> 
   filter(str_detect(word, "\\W", negate = TRUE)) |> 
   mutate(word = textstem::stem_words(word)) |> 
   {\(.) left_join(., count(., word), by = "word")}() |> 
   filter(n > 3) |> 
   select(-n)
-             
+          
 
 # Bag of words ------------------------------------------------------------
 
-# Dummy cols
+# tf_idf
+tfidf <- left_join(hashtag_df, word_df, by ="status_id") |> 
+  select(-status_id) |> 
+  na.omit() |> 
+  with_groups(c(hashtag, word), ~ summarise(.x, n = n())) |> 
+  bind_tf_idf(word, hashtag, n) |> 
+  anti_join(stop_words, by = "word") ###
+
+# cosine similarity
+(similarity <- pairwise_similarity(tfidf, hashtag, word, tf_idf) |> 
+  na.omit() |> 
+  filter(similarity >= 0.05) |> 
+  arrange(item1, desc(similarity))) |> 
+  group_by(item1) |> 
+  slice_max(similarity) |> 
+  view("")
+
+km <- widely_kmeans(similarity, item1, item2, similarity, k = 20, fill = 0)
+count(km, cluster)
